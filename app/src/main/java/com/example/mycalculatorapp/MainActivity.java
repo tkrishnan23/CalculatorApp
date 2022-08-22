26a33522a57d4e1bde554551d17ef8c67d74cc6c
tkrishnan23 <90794124+tkrishnan23@users.noreply.github.com>
2022-08-22 08:19:07 -0500

    debugged my functions

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -15,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void addNums(View view){
-        EditText firstNum = findViewById(R.id.userFirstNum);
-        EditText secondNum = findViewById(R.id.userSecondNum);
+        EditText firstNum = (EditText)findViewById(R.id.firstNum);
+        EditText secondNum = (EditText)findViewById(R.id.secondNum);
 
         //I got this from https://www.tutorialspoint.com/java/number_parseint.htm
         int first =Integer.parseInt(firstNum.getText().toString());
@@ -25,8 +26,50 @@ public class MainActivity extends AppCompatActivity {
 
         int sum = first + second;
 
-        TextView tv_data=(TextView)findViewById(R.id.sumTV);
+        TextView tv_data=(TextView)findViewById(R.id.answerTV);
         tv_data.setText(sum);
 
+    }
+
+    public void subNums(View view){
+        EditText firstNum = (EditText)findViewById(R.id.firstNum);
+        EditText secondNum = (EditText)findViewById(R.id.secondNum);
+
+        //I got this from https://www.tutorialspoint.com/java/number_parseint.htm
+        int first =Integer.parseInt(firstNum.getText().toString());
+        int second =Integer.parseInt(secondNum.getText().toString());
+
+        int difference = first - second;
+
+        TextView tv_data=(TextView)findViewById(R.id.answerTV);
+        tv_data.setText(difference);
+    }
+
+    public void multNums(View view){
+        EditText firstNum = (EditText)findViewById(R.id.firstNum);
+        EditText secondNum = (EditText)findViewById(R.id.secondNum);
+
+        //I got this from https://www.tutorialspoint.com/java/number_parseint.htm
+        int first =Integer.parseInt(firstNum.getText().toString());
+        int second =Integer.parseInt(secondNum.getText().toString());
+
+        int product = first * second;
+
+        TextView tv_data=(TextView)findViewById(R.id.answerTV);
+        tv_data.setText(product);
+    }
+
+    public void divNums(View view){
+        EditText firstNum = (EditText)findViewById(R.id.firstNum);
+        EditText secondNum = (EditText)findViewById(R.id.secondNum);
+
+        //I got this from https://www.tutorialspoint.com/java/number_parseint.htm
+        int first =Integer.parseInt(firstNum.getText().toString());
+        int second =Integer.parseInt(secondNum.getText().toString());
+
+        int div = first/second;
+
+        TextView tv_data=(TextView)findViewById(R.id.answerTV);
+        tv_data.setText(div);
     }
 }
